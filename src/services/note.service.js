aac82e43f18b92094e85e4562262eaa0b14fd389
akhilmondal <akhilmandalfamily@gmail.com>
2023-08-21 16:50:52 +0530

    [Akhil] Added: code for update note with note id.

--- src/services/note.service.js
+++ src/services/note.service.js
@@ -1,6 +1,7 @@
 import { Note } from '../config/firestore';
 import noteModel from '../models/note.model';
 
+//Service for create note
 export const newNote = async (
   id,
   title,
@@ -27,4 +28,15 @@ export const newNote = async (
   //passing that whole data into the function to filter out only required data.
   const addedNote = noteModel.getNoteFromFirestore(docSnapshot);
   return addedNote;
+};
+
+//service for update note
+export const updateNoteById = async (id, body) => {
+  //searching for the document with id and updating with new data of body.
+  await Note.doc(id).update(body);
+  //Fetching the whole data.
+  const docSnapshot = await Note.doc(id).get();
+  //formating it for a proper response.
+  const updatedNote = noteModel.getNoteFromFirestore(docSnapshot);
+  return updatedNote;
 };
